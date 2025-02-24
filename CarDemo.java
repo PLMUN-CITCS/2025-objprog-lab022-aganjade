6a40d3d0eaf8883a222180350b324d0045e1538b
Agan Jade <aganjade_bsit@plmun.edu.ph>
2025-02-24 13:59:07 +0800

    Submitting OBJPROG Week 05 - Laboratory # 22

---
+++ CarDemo.java
@@ -0,0 +1,25 @@
+class Car {
+class Car {
+    String make;
+    String model;
+    int year;
+}
+class Car {
+    //... (attributes)...
+
+    public void displayInfo() {
+        System.out.println("Car: " + year + " " + make + " " + model);
+    }
+}
+public class CarDemo {
+    public static void main(String[] args) {
+        // Code will go here
+    }
+}
+Car myCar = nemyCar.make = "Toyota";
+myCar.model = "Corolla";
+myCar.year = 2020;w Car();
+myCar.displayInfo();
+
+
+}
